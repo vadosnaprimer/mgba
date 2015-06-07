ecfd177f8bc141b51493ac85cbd99d2dcb975476
zeromus <zeromus@users.noreply.github.com>
2015-06-06 21:18:38 -0500

    add visual studio 2015 building including macro preprocessor tools

---
+++ src/platform/bizhawk/vs2015/precompile/precompile.bat
@@ -0,0 +1,3 @@
+msvc-gcc-precompiler %1 ..\..\..\..\arm\isa-thumb.c isa-thumb.i.c 2>NUL
+msvc-gcc-precompiler %1 ..\..\..\..\arm\decoder-thumb.c decoder-thumb.i.c 2>NUL
+msvc-gcc-precompiler %1 ..\..\..\..\arm\isa-arm.c isa-arm.i.c 2>NUL
