afb27926fd6cc02aae56cb22b7b769acee4979df
zeromus <zeromus@users.noreply.github.com>
2015-06-21 18:50:10 -0500

    vs2015 - remove precompiler hacks since they're no longer needed

--- src/platform/bizhawk/vs2015/precompile/precompile.bat
+++
@@ -1,3 +0,0 @@
-msvc-gcc-precompiler %1 ..\..\..\..\arm\isa-thumb.c isa-thumb.i.c 2>NUL
-msvc-gcc-precompiler %1 ..\..\..\..\arm\decoder-thumb.c decoder-thumb.i.c 2>NUL
-msvc-gcc-precompiler %1 ..\..\..\..\arm\isa-arm.c isa-arm.i.c 2>NUL
